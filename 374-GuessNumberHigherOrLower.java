280d31e64a2abc5e6fc2e26dd99b9ef093278667
Zhou-Duo <38467444+Zhou-Duo@users.noreply.github.com>
2021-12-31 06:24:32 +0800

    374-GuessNumberHigherOrLower

---
+++ 374-GuessNumberHigherOrLower.java
@@ -0,0 +1,27 @@
+class Solution {
+    int guess(int num) {
+        /**
+         * @param num your guess
+         * @return -1 if num is lower than the guess number
+         *         1 if num is higher than the guess number
+         *         otherwise return 0
+         */
+        return 0;
+    }
+
+    public int guessNumber(int n) {
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (guess(mid) == 0)
+                return mid;
+            else if (guess(mid) == -1)
+                right = mid - 1;
+            else if (guess(mid) == 1)
+                left = mid + 1;
+        }
+        return left;
+    }
+
+}
