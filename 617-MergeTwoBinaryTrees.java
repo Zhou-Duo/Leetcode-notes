6196be5c61f3b4f418cb02a08127b4419527d6d9
Zhou-Duo <38467444+Zhou-Duo@users.noreply.github.com>
2022-01-11 23:45:57 +0800

    617-MergeTwoBinaryTrees

--- 617-MergeTwoBinaryTrees.java
+++ 617-MergeTwoBinaryTrees.java
@@ -10,7 +10,6 @@ class TreeNode {
         this.right = right;
     }
 }
-
 /* 
  * 可以使用深度优先搜索合并两个二叉树。从根节点开始同时遍历两个二叉树，并将对应的节点进行合并。
  * 两个二叉树的对应节点可能存在以下三种情况，对于每种情况使用不同的合并方式。
@@ -19,18 +18,19 @@ class TreeNode {
  * 如果两个二叉树的对应节点都不为空，则合并后的二叉树的对应节点的值为两个二叉树的对应节点的值之和，此时需要显性合并两个节点。
  * 对一个节点进行合并之后，还要对该节点的左右子树分别进行合并。这是一个递归的过程。
  */
+
 class Solution {
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if (t1 == null) {
-            return t2;
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
         }
-        if (t2 == null) {
-            return t1;
+        if (root2 == null) {
+            return root1;
         }
-        TreeNode merged = new TreeNode(t1.val + t2.val);
-        merged.left = mergeTrees(t1.left, t2.left);
-        merged.right = mergeTrees(t1.right, t2.right);
-        return merged;
+        TreeNode mergedRoot = new TreeNode(root1.val + root2.val);
+        mergedRoot.left = mergeTrees(root1.left,root2.left);
+        mergedRoot.right = mergeTrees(root1.right, root2.right);
+        return mergedRoot;
     }
 }
 /* 
