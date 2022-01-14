27bc0ce90ce76bba4332262e9d39a72c99c2a9c5
Zhou-Duo <38467444+Zhou-Duo@users.noreply.github.com>
2022-01-14 23:53:11 +0800

    Placeholder

--- 46-Permutations.java
+++ 46-Permutations.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
