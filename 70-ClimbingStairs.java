c31cea1606864783a4b4429b62955587e3ec187a
Zhou-Duo <38467444+Zhou-Duo@users.noreply.github.com>
2022-01-21 18:36:44 +0800

    70-ClimbingStairs

--- 70-ClimbingStairs.java
+++ 70-ClimbingStairs.java
@@ -1,6 +1,25 @@
-class Solution {
+// 1. 动态规划
+class Solution1 {
     public int climbStairs(int n) {
-        return 0;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
 
+// 2. 优化空间复杂度
+class Solution2 {
+    public int climbStairs(int n) {
+        int a = 0, b = 0, c = 1;
+        for (int i = 0; i < n; i++) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return c;
     }
 }
