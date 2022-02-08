2464d9bbcbe5e8c693bb5dde9e396b3546b6f349
Zhou-Duo <xzhouaz@connect.ust.hk>
2022-02-08 18:55:00 +0800

    1-TwoSum

---
+++ 1-TwoSum.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+ * 复杂度分析
+ * 时间复杂度：O(N)，其中 N 是数组中的元素数量。对于每一个元素 x，我们可以 O(1) 地寻找 target - x。
+ * 空间复杂度：O(N)，其中 N 是数组中的元素数量。主要为哈希表的开销。
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            // 先check后put！
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
