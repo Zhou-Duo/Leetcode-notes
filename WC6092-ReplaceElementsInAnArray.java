c18e68d263460d0bc195ae24cd3e02c9d5a98211
Zhou-Duo <xzhouaz@connect.ust.hk>
2022-06-08 14:26:43 +0800

    WC6092-ReplaceElementsInAnArray

---
+++ WC6092-ReplaceElementsInAnArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] arrayChange(int[] nums, int[][] operations) {
+        int[] delta = new int[1000001];
+        for (int i = 0; i < nums.length; i++) {
+            delta[nums[i]] = i;
+        }
+        for (int[] operation : operations) {
+            delta[operation[1]] = delta[operation[0]];
+            nums[delta[operation[1]]] = operation[1];
+        }
+        return nums;
+    }
+}
