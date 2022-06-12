811da84acd8235edb0dc9696b25011e37bf4dfd2
Zhou-Duo <xzhouaz@connect.ust.hk>
2022-06-13 01:45:43 +0800

    2-两数相加

---
+++ 2-两数相加.java
@@ -0,0 +1,34 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+// 1. 迭代
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+            if(l1 != null)
+                l1 = l1.next;
+            if(l2 != null)
+                l2 = l2.next;
+        }
+        if (carry > 0)
+            cur.next = new ListNode(carry);
+        return dummy.next;
+    }
+}
